96755e51ed3bd431f69b115524bfe8cc96de640a
Jesús Gutiérrez Matarrita <jesusantonio.gutierrezmatarrita@gmail.com>
2022-06-17 17:46:15 -0600

    June, 6th classes

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { StudentServiceService } from '../student-service.service';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * An array which receives any type of object
+   */
+  students:any = [];
+
+  constructor(public rest: StudentServiceService, private route: ActivatedRoute, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  /**
+   * Gets all the students from the service
+   */
+  getStudents() {
+    this.students = [];
+
+    this.rest.getStudent().subscribe((data: {}) => {
+      console.log(data);
+
+      this.students = data;
+    });
+  }
+
+  /**
+   * Adds a student
+   */
+  add() {
+
+  }
+
+  /**
+   * Deletes a student
+   */
+  delete(id:number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.rest.delete(id).subscribe((data) => {
+          console.log(data);
+
+          this.ngOnInit();
+        })
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'The student has been deleted',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
   }
 
 }
--- src/app/student-service.service.ts
+++ src/app/student-service.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+
+const endpoint = 'https://localhost:7151/api/';
+const httpOptions = {
+  headers: new HttpHeaders ({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStudent(): Observable<any> {
+    return this.http.get(endpoint + 'Students/GetStudents', httpOptions);
+  }
+
+  delete(id:number): Observable<any> {
+    return this.http.delete(endpoint + 'Students/DeleteStudent/' + id, httpOptions)
+    .pipe(
+      catchError(this.handleError('deleteStudent'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
